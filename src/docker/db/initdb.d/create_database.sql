cc0015382df0b190ab7c544ba0ac541946fcc8c4
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2021-06-04 11:06:18 +0900

    fix: SQL修正

--- src/docker/db/initdb.d/create_database.sql
+++ src/docker/db/initdb.d/create_database.sql
@@ -1,7 +1,4 @@
 -- 初期DBを作成
--- https://qiita.com/NagaokaKenichi/items/ae037963b33a85df33f5
--- https://www.dbonline.jp/mysql/user/index1.html
--- https://www.dbonline.jp/mysql/user/index6.htmls
 CREATE DATABASE IF NOT EXISTS `db_local`;
 CREATE USER 'db_local'@'%' IDENTIFIED BY 'db_local';
 GRANT ALL ON db_local.* TO 'db_local'@'%';
