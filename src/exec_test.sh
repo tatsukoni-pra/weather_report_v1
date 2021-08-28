cf8d9fb42955f4b9c53362701784a01028865e13
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2021-08-29 00:13:30 +0900

    add: ローカルでテスト実行するまで

---
+++ src/exec_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 実行させたいテストファイルを引数に渡す
+execFile=`echo $1 | sed s/src/./`
+php ./vendor/bin/phpunit $execFile
---
+++ src/tests/Feature/Controllers/RainControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class RainControllerTest extends TestCase
+{
+    /**
+     * 基本的なテスト例
+     *
+     * @return void
+     */
+    public function testIndev()
+    {
+        $this->assertTrue(true);
+    }
+}
